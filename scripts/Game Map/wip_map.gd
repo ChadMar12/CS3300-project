b814c4dc44b54e9deae4d4aa0a6e668508b708e7
ChadMar12 <chadmarshall50@gmail.com>
2025-03-22 03:12:50 -0600

    Level Switching Code working
    - Level switching code is working for portal.
    - Two main files added.
    1. level.gd - This file if the main logic for switching levels
    2. level_manager.gd - This file holds a the main scene as a global variable to make switching back and forth smooth.

--- scripts/Game Map/wip_map.gd
+++ scripts/Game Map/wip_map.gd
@@ -1,9 +1,4 @@
-
 extends Level
 
-
-
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print('Player Has entered Portal')
 	switchLevel('Dungeon1')
--- scripts/level.gd
+++ scripts/level.gd
@@ -2,9 +2,47 @@ class_name Level
 extends Node
 
 const Scene = {
-	'Dungeon1': "res://scene/Dungeon/Dungeon.tscn",
-	'MainWorld' : "res://scene/game map/game_world.tscn"
+	'MainWorld': "res://scene/game map/game_world.tscn",
+	'Dungeon1': "res://scene/Dungeon/Dungeon.tscn"
 }
 
+func _ready():
+	
+	if level_manager.main_world == null:
+		var main_scene = load(Scene['MainWorld'])
+		level_manager.main_world = main_scene.instantiate()
+		get_tree().get_root().add_child(level_manager.main_world)
+
 func switchLevel(target: String):
-	get_tree().change_scene_to_file(Scene[target])
+	var tree = get_tree()
+	var root = tree.get_root()
+	var current_scene = tree.current_scene
+
+	if current_scene:
+		root.remove_child(current_scene)
+		
+	if target == "MainWorld":
+
+		if level_manager.main_world:
+			root.add_child(level_manager.main_world)
+			tree.set_current_scene(level_manager.main_world)
+		else:
+		
+			var main_scene = load(Scene['MainWorld'])
+			level_manager.main_world = main_scene.instantiate()
+			root.add_child(level_manager.main_world)
+			tree.set_current_scene(level_manager.main_world)
+		
+
+		if current_scene and current_scene != level_manager.main_world:
+			current_scene.queue_free()
+	else:
+
+		if current_scene == level_manager.main_world:
+			root.remove_child(level_manager.main_world)
+		else:
+			current_scene.queue_free()
+
+		var new_scene = load(Scene[target]).instantiate()
+		root.add_child(new_scene)
+		tree.set_current_scene(new_scene)
