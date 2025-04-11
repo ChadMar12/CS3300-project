d3962ca28edec248cb3e077d257b096c306eb08f
ChadMar12 <chadmarshall50@gmail.com>
2025-04-11 10:49:07 -0600

    Finished character ui selection screen
    Finished character selection screen
    Need to fix main spawn

--- scripts/Characters/character_selection.gd
+++ scripts/Characters/character_selection.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var player_label = $player_description
 
 func _on_knight_pressed() -> void:
 	level_manager.character_selected = 'Knight'
@@ -28,40 +29,80 @@ func _on_druid_pressed() -> void:
 
 func _on_knight_mouse_entered() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Cheer')
+	player_label.show()
+	player_label.set_text('   Description:
+							
+							  Combat Type:
+							  Health:
+							  Armor:
+							  Weapon:')
 
 
 
 func _on_knight_mouse_exited() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Idle')
+	player_label.hide()
 
 
 func _on_mage_mouse_entered() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Cheer')
+	player_label.show()
+	player_label.set_text('   Description:
+							
+							  Combat Type:
+							  Health:
+							  Armor:
+							  Weapon:')
 
 
 func _on_mage_mouse_exited() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Idle')
+	player_label.hide()
 
 
 func _on_barbarian_mouse_entered() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Cheer')
+	player_label.show()
+	player_label.set_text('   Description:
+							
+							  Combat Type:
+							  Health:
+							  Armor:
+							  Weapon:')
 
 
 func _on_barbarian_mouse_exited() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Idle')
+	player_label.hide()
 
 
 func _on_rouge_mouse_entered() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Cheer')
+	player_label.show()
+	player_label.set_text('   Description:
+							
+							  Combat Type:
+							  Health:
+							  Armor:
+							  Weapon:')
 
 
 func _on_rouge_mouse_exited() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Idle')
+	player_label.hide()
 
 
 func _on_druid_mouse_entered() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Cheer')
+	player_label.show()
+	player_label.set_text('   Description:
+							
+							  Combat Type:
+							  Health:
+							  Armor:
+							  Weapon:')
 
 
 func _on_druid_mouse_exited() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Idle')
+	player_label.hide()
