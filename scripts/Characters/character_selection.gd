07504ebe27160bb320219d981b4b52b84ae460a2
ChadMar12 <chadmarshall50@gmail.com>
2025-04-18 09:05:16 -0600

    Moved code around

--- scripts/Characters/character_selection.gd
+++ scripts/Characters/character_selection.gd
@@ -2,30 +2,8 @@ extends Control
 
 @onready var player_label = $player_description
 
-func _on_knight_pressed() -> void:
-	level_manager.character_selected = 'Knight'
-	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
-
-func _on_mage_pressed() -> void:
-	level_manager.character_selected = 'Mage'
-	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
-
-
-func _on_barbarian_pressed() -> void:
-	level_manager.character_selected = 'Barbarian'
-	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
-
-
-func _on_rouge_pressed() -> void:
-	level_manager.character_selected = 'Rouge'
-	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
-
-
-func _on_druid_pressed() -> void:
-	level_manager.character_selected = 'Druid'
-	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
-
-
+## Functions for Knight to handle when the mouse enters the button, the animation " Cheer " will play and the player's label
+## will show the information about the character.
 func _on_knight_mouse_entered() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Cheer')
 	player_label.show()
@@ -36,13 +14,17 @@ func _on_knight_mouse_entered() -> void:
 							  Armor:
 							  Weapon:')
 
-
-
 func _on_knight_mouse_exited() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Idle')
 	player_label.hide()
 
-
+func _on_knight_pressed() -> void:
+	level_manager.character_selected = 'Knight'
+	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
+	
+	
+## Functions for mage to handle when the mouse enters the button, the animation " Cheer " will play and the player's label
+## will show the information about the character.
 func _on_mage_mouse_entered() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Cheer')
 	player_label.show()
@@ -53,12 +35,17 @@ func _on_mage_mouse_entered() -> void:
 							  Armor:
 							  Weapon:')
 
-
 func _on_mage_mouse_exited() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Idle')
 	player_label.hide()
 
+func _on_mage_pressed() -> void:
+	level_manager.character_selected = 'Mage'
+	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
+	
 
+## Functions for barbarian to handle when the mouse enters the button, the animation " Cheer " will play and the player's label
+## will show the information about the character.
 func _on_barbarian_mouse_entered() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Cheer')
 	player_label.show()
@@ -69,12 +56,17 @@ func _on_barbarian_mouse_entered() -> void:
 							  Armor:
 							  Weapon:')
 
-
 func _on_barbarian_mouse_exited() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Idle')
 	player_label.hide()
+	
+func _on_barbarian_pressed() -> void:
+	level_manager.character_selected = 'Barbarian'
+	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
 
 
+## Functions for  rouge to handle when the mouse enters the button, the animation " Cheer " will play and the player's label
+## will show the information about the character.
 func _on_rouge_mouse_entered() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Cheer')
 	player_label.show()
@@ -85,12 +77,17 @@ func _on_rouge_mouse_entered() -> void:
 							  Armor:
 							  Weapon:')
 
-
 func _on_rouge_mouse_exited() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Idle')
 	player_label.hide()
 
+func _on_rouge_pressed() -> void:
+	level_manager.character_selected = 'Rouge'
+	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
 
+
+## Functions for druid to handle when the mouse enters the button, the animation " Cheer " will play and the player's label
+## will show the information about the character.
 func _on_druid_mouse_entered() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Cheer')
 	player_label.show()
@@ -101,7 +98,10 @@ func _on_druid_mouse_entered() -> void:
 							  Armor:
 							  Weapon:')
 
-
 func _on_druid_mouse_exited() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Idle')
 	player_label.hide()
+
+func _on_druid_pressed() -> void:
+	level_manager.character_selected = 'Druid'
+	get_tree().change_scene_to_file('res://scene/ui/loading_scene.tscn')
--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -6,6 +6,5 @@ func _on_start_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/ui/settings_menu.tscn")
