f014c924e590d18a1c9ae44a3946f75fabd610ae
casperpool <casperpool1234@gmail.com>
2025-04-21 14:33:04 -0600

    added character descriptions

--- scripts/Characters/character_selection.gd
+++ scripts/Characters/character_selection.gd
@@ -7,12 +7,13 @@ extends Control
 func _on_knight_mouse_entered() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Cheer')
 	player_label.show()
-	player_label.set_text('   Description:
+	player_label.set_text('   Description: A disciplined warrior clad in heavy armor, the Knight excels 
+							  in close combat and battlefield resilience. 
 							
-							  Combat Type:
-							  Health:
-							  Armor:
-							  Weapon:')
+							  Combat Type: Melee
+							  Health: 100
+							  Armor: 50
+							  Weapon: Sword')
 
 func _on_knight_mouse_exited() -> void:
 	$HBoxContainer/Knight/knight_preview/SubViewportContainer/SubViewport/Knight.set_move_state('Idle')
@@ -28,12 +29,13 @@ func _on_knight_pressed() -> void:
 func _on_mage_mouse_entered() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Cheer')
 	player_label.show()
-	player_label.set_text('   Description:
+	player_label.set_text('   Description: A master of arcane arts cloaked in mystical robes, the Mage 
+							  excels in long-range combat and elemental power.
 							
-							  Combat Type:
-							  Health:
-							  Armor:
-							  Weapon:')
+							  Combat Type: Long Range Spell Bearer
+							  Health: 75
+							  Armor: 0
+							  Weapon: Staff')
 
 func _on_mage_mouse_exited() -> void:
 	$HBoxContainer/Mage/mage_preview/SubViewportContainer/SubViewport/Mage.set_move_state('Idle')
@@ -49,12 +51,13 @@ func _on_mage_pressed() -> void:
 func _on_barbarian_mouse_entered() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Cheer')
 	player_label.show()
-	player_label.set_text('   Description:
+	player_label.set_text('   Description: A fierce warrior driven by raw strength and primal fury, 
+							  the Barbarian excels in brutal melee combat and relentless aggression.
 							
-							  Combat Type:
-							  Health:
-							  Armor:
-							  Weapon:')
+							  Combat Type: Melee
+							  Health: 125
+							  Armor: 0
+							  Weapon: Axe')
 
 func _on_barbarian_mouse_exited() -> void:
 	$HBoxContainer/Barbarian/barbarian_preview/SubViewportContainer/SubViewport/Barbarian.set_move_state('Idle')
@@ -70,12 +73,13 @@ func _on_barbarian_pressed() -> void:
 func _on_rouge_mouse_entered() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Cheer')
 	player_label.show()
-	player_label.set_text('   Description:
+	player_label.set_text('   Description: A cunning fighter shrouded in shadow, the Rogue excels in 
+							  stealth, agility, and precision strikes.
 							
-							  Combat Type:
-							  Health:
-							  Armor:
-							  Weapon:')
+							  Combat Type: Combo
+							  Health: 100
+							  Armor: 0
+							  Weapon: CrossBow & Knife')
 
 func _on_rouge_mouse_exited() -> void:
 	$HBoxContainer/Rouge/rouge_preview/SubViewportContainer/SubViewport/Rouge.set_move_state('Idle')
@@ -91,12 +95,13 @@ func _on_rouge_pressed() -> void:
 func _on_druid_mouse_entered() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Cheer')
 	player_label.show()
-	player_label.set_text('   Description:
+	player_label.set_text('   Description: A guardian of nature and master of elemental balance, 
+							  The Druid excels in adaptive magic and healing abilities.
 							
-							  Combat Type:
-							  Health:
-							  Armor:
-							  Weapon:')
+							  Combat Type: Long Range Utility Spell Bearer
+							  Health: 50
+							  Armor: 25
+							  Weapon: Healing Staff')
 
 func _on_druid_mouse_exited() -> void:
 	$HBoxContainer/Druid/druid_preview/SubViewportContainer/SubViewport/Druid.set_move_state('Idle')
