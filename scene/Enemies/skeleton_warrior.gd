f0e21c35c6715b059721655bcc58115de4bbe248
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2025-04-22 08:30:25 -0600

    Skeleton_warrior animation

---
+++ scene/Enemies/skeleton_warrior.gd
@@ -0,0 +1,11 @@
+extends Enemy
+
+func _physics_process(delta: float) -> void:
+	move_to_player(delta)
+
+func _on_attack_timer_timeout() -> void:
+	$Node/AttackTimer.wait_time = rng.randf_range(2.5, 3.5)
+	if position.distance_to(player.position) < attack_radius:
+		print("in_radius")
+		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		
